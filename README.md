1b86f20be3dc89f33565b6d45f975c90c66be307
Piyush Kansal <piyush@netpowerandlight.com>
2017-01-17 22:01:22 -0500

    Added feature to supply multiple ports or port range. And code cleanup

--- README.md
+++ README.md
@@ -3,57 +3,100 @@
 ## Usage
 
 ```
-wait-for-it.sh host:port [-s] [-t timeout] [-- command args]
--h HOST | --host=HOST       Host or IP under test
--p PORT | --port=PORT       TCP port under test
-                            Alternatively, you specify the host and port as host:port
--s | --strict               Only execute subcommand if the test succeeds
--q | --quiet                Don't output any status messages
--t TIMEOUT | --timeout=TIMEOUT
-                            Timeout in seconds, zero for no timeout
--- COMMAND ARGS             Execute command with args after the test finishes
+wait-for-it.sh host1:port1:sleep1 host2:port2:sleep2 [-s] [-q] [-t timeout] [-- command args]
+host:port:sleep         Host and port of the service to wait for. Once the port is available, may want to sleep to 
+                        ensure the application is "ready" (in secs, optional, default 3s). Do not like to sleep? Pass 0.
+                        To wait for multiple hosts, pass more hosts separated by space eg. h1:p1:s1 h2:p2:s2
+                        To wait for multiple ports on the same host, pass ports separated by comma eg. h1:p1,p2,p3:s1
+                        To wait for a port range on the same host, pass ports separated by hypen eg. h1:p1-pN:s1
+-s                      Only execute subcommand if the test succeeds
+-q                      Do not output any status messages
+-t TIMEOUT              Timeout in seconds, zero for no timeout (honours sleep time when t > 0)
+-- COMMAND ARGS         Execute command with args after the test finishes
 ```
 
 ## Examples
 
-For example, let's test to see if we can access port 80 on www.google.com, and if it is available, echo the message `google is up`.
+To see if we can access port 80 on www.google.com and echo the message `google is up`.
 
 ```
 $ ./wait-for-it.sh www.google.com:80 -- echo "google is up"
-wait-for-it.sh: waiting 15 seconds for www.google.com:80
+wait-for-it.sh: waiting 14 seconds for www.google.com:80
 wait-for-it.sh: www.google.com:80 is available after 0 seconds
+wait-for-it.sh: sleeping for 3s to ensure service is "ready"
 google is up
 ```
 
-You can set your own timeout with the `-t` or `--timeout=` option.  Setting the timeout value to 0 will disable the timeout:
+To see if we can access port 5432 on postgres and wait for 10s for postgres table initialization to complete and start the app server.
+
+```
+$ ./wait-for-it.sh postgres:5432:10 -- appServer.py start
+wait-for-it.sh: waiting 21 seconds for postgres:5432
+wait-for-it.sh: postgres:5432 is available after 0 seconds
+wait-for-it.sh: sleeping for 10s to ensure service is "ready"
+appServer started.
+```
+
+To see if we can access ports 2181, 2888, 3888 on zookeeper, do not sleep after the port check is complete and do no execute a subcommand. Useful when you want to refer to your containers with names instead of ip and have multiple ports to check on that container. So, instead of writing `zookeeper:2181 zookeeper:2888 zookeeper:3888`, you can keep it clean via `zookeeper:2181,2888,3888`.
+
+```
+$ ./wait-for-it.sh zookeeper:2181,2888,3888:0
+wait-for-it.sh: waiting 11 seconds for zookeeper:2181
+wait-for-it.sh: zookeeper:2181 is available after 0 seconds
+wait-for-it.sh: waiting 11 seconds for zookeeper:2888
+wait-for-it.sh: zookeeper:2888 is available after 1 seconds
+wait-for-it.sh: waiting 11 seconds for zookeeper:3888
+wait-for-it.sh: zookeeper:3888 is available after 2 seconds
+```
+
+To see if we can access ports 6379-6390 on redis and wait for 5s for redis to get initialized with some data. Useful when you want to refer to your containers with names instead of ip and have a port range to check on that container. So, instead of writing `redis:6379:5 redis:6380:5 ... redis:6390:5`, you can keep it clean via `redis:6379-6390:5`.
+
+```
+$ ./wait-for-it.sh redis:6379-6390:5
+wait-for-it.sh: waiting 16 seconds for redis:6379
+wait-for-it.sh: redis:6379 is available after 0 seconds
+wait-for-it.sh: sleeping for 5s to ensure service is "ready"
+...
+wait-for-it.sh: waiting 16 seconds for redis:6390
+wait-for-it.sh: redis:6390 is available after 1 seconds
+wait-for-it.sh: sleeping for 5s to ensure service is "ready"
+```
+
+You can set your own timeout with the `-t` option. Setting the timeout value to 0 will disable the timeout:
 
 ```
 $ ./wait-for-it.sh -t 0 www.google.com:80 -- echo "google is up"
 wait-for-it.sh: waiting for www.google.com:80 without a timeout
 wait-for-it.sh: www.google.com:80 is available after 0 seconds
+wait-for-it.sh: sleeping for 3s to ensure service is "ready"
 google is up
 ```
 
-The subcommand will be executed regardless if the service is up or not.  If you wish to execute the subcommand only if the service is up, add the `--strict` argument. In this example, we will test port 81 on www.google.com which will fail:
+The subcommand will be executed regardless if the service is up or not. If you wish to execute the subcommand only if the service is up, add the `-s` argument. In this example, we will test port 81 on www.google.com which will fail:
 
 ```
-$ ./wait-for-it.sh www.google.com:81 --timeout=1 --strict -- echo "google is up"
+$ ./wait-for-it.sh www.google.com:81:0 -t 1 -s -- echo "google is up"
 wait-for-it.sh: waiting 1 seconds for www.google.com:81
 wait-for-it.sh: timeout occurred after waiting 1 seconds for www.google.com:81
 wait-for-it.sh: strict mode, refusing to execute subprocess
 ```
 
-If you don't want to execute a subcommand, leave off the `--` argument.  This way, you can test the exit condition of `wait-for-it.sh` in your own scripts, and determine how to proceed:
+If you don't want to execute a subcommand, leave off the `--` argument. This way, you can test the exit condition of `wait-for-it.sh` in your own scripts, and determine how to proceed:
 
 ```
 $ ./wait-for-it.sh www.google.com:80
-wait-for-it.sh: waiting 15 seconds for www.google.com:80
+wait-for-it.sh: waiting 14 seconds for www.google.com:80
 wait-for-it.sh: www.google.com:80 is available after 0 seconds
 $ echo $?
 0
 $ ./wait-for-it.sh www.google.com:81
-wait-for-it.sh: waiting 15 seconds for www.google.com:81
-wait-for-it.sh: timeout occurred after waiting 15 seconds for www.google.com:81
+wait-for-it.sh: waiting 14 seconds for www.google.com:81
+wait-for-it.sh: timeout occurred after waiting 14 seconds for www.google.com:81
 $ echo $?
 124
+```
+
+Why `3` and `11` as sleep and timeout respectively?
+```
+I just love prime numbers!
 ```
--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-#   Use this script to test if a given TCP host/port are available
+
+# Use this script to test if a given TCP host/port are available
 
 cmdname=$(basename $0)
 
@@ -8,138 +9,170 @@ echoerr() { if [[ $QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
 usage() {
     cat << USAGE >&2
 Usage:
-    $cmdname host:port [-s] [-t timeout] [-- command args]
-    -s | --strict               Only execute subcommand if the test succeeds
-    -q | --quiet                Don't output any status messages
-    -t TIMEOUT | --timeout=TIMEOUT
-                                Timeout in seconds, zero for no timeout
-    -- COMMAND ARGS             Execute command with args after the test finishes
+    $cmdname host:port:sleep [-s] [-q] [-t timeout] [-- command args]
+    host:port:sleep         Host and port of the service to wait for.
+                            Sleep after the port is up to ensure application is "ready" (in secs, optional, default 3s).
+                            Do not like to sleep? Pass 0.
+    -s                      Only execute subcommand if the test succeeds
+    -q                      Do not output any status messages
+    -t TIMEOUT              Timeout in seconds, zero for no timeout (honours sleep time when t > 0)
+    -- COMMAND ARGS         Execute command with args after the test finishes
 USAGE
     exit 1
 }
 
 wait_for() {
     local wait_host=$1
-    local wait_port=$2
+    local wait_ports=$2
+    local sleep_time=$3
+
+    case "${wait_ports}" in
+        *-*)
+        wait_ports=(${wait_ports//-/ })
+        wait_ports=($(seq ${wait_ports[0]} ${wait_ports[1]}))
+        ;;
+        *,*)
+        wait_ports=(${wait_ports//,/ })
+        ;;
+        *)
+        wait_ports=(${wait_ports})
+        ;;
+    esac
+
     if [[ $TIMEOUT -gt 0 ]]; then
-        echoerr "$cmdname: waiting $TIMEOUT seconds for $wait_host:$wait_port"
+        echoerr "$cmdname: waiting $((TIMEOUT + sleep_time)) seconds for $wait_host:${wait_ports[@]}"
     else
-        echoerr "$cmdname: waiting for $wait_host:$wait_port without a timeout"
+        echoerr "$cmdname: waiting for $wait_host:${wait_ports[@]} without a timeout"
     fi
+
     local start_ts=$(date +%s)
-    while :
+    for wait_port in ${wait_ports[@]}
     do
-        (echo > /dev/tcp/$wait_host/$wait_port) >/dev/null 2>&1
-        local result=$?
-        if [[ $result -eq 0 ]]; then
-            local end_ts=$(date +%s)
-            echoerr "$cmdname: $wait_host:$wait_port is available after $((end_ts - start_ts)) seconds"
-            break
-        fi
-        sleep 1
+        while :
+        do
+            (echo > /dev/tcp/$wait_host/$wait_port) >/dev/null 2>&1
+            local result=$?
+            if [[ $result -eq 0 ]]; then
+                local end_ts=$(date +%s)
+                echoerr "$cmdname: $wait_host:$wait_port is available after $((end_ts - start_ts)) seconds"
+                break
+            fi
+            sleep 1
+        done
     done
+
+    # Sleep for extra time to make sure that the application is thoroughly started and is "ready" to process requests
+    echoerr "$cmdname: sleeping for ${sleep_time}s to ensure service is \"ready\""
+    sleep $sleep_time
+
     return $result
 }
 
 wait_for_wrapper() {
     local wait_host=$1
     local wait_port=$2
+    local sleep_time=$3
+
     # In order to support SIGINT during timeout: http://unix.stackexchange.com/a/57692
     if [[ $QUIET -eq 1 ]]; then
-        timeout $TIMEOUT $0 $wait_host:$wait_port --quiet --child --timeout=$TIMEOUT &
+        timeout $((TIMEOUT + sleep_time)) $0 $wait_host:$wait_port:$sleep_time -q -c -t $TIMEOUT &
     else
-        timeout $TIMEOUT $0 $wait_host:$wait_port --child --timeout=$TIMEOUT &
+        timeout $((TIMEOUT + sleep_time)) $0 $wait_host:$wait_port:$sleep_time -c -t $TIMEOUT &
     fi
+
     PID=$!
     trap "kill -INT -$PID" INT
+
     wait $PID
     RESULT=$?
     if [[ $RESULT -ne 0 ]]; then
-        echoerr "$cmdname: timeout occurred after waiting $TIMEOUT seconds for $wait_host:$wait_port"
+        echoerr "$cmdname: timeout occurred after waiting $((TIMEOUT + sleep_time)) seconds for $wait_host:$wait_port"
     fi
+
     return $RESULT
 }
 
 parse_arguments() {
-  local index=0
-  while [[ $# -gt 0 ]]
-  do
-      case "$1" in
-          *:* )
-          hostport=(${1//:/ })
-          HOST[$index]=${hostport[0]}
-          PORT[$index]=${hostport[1]}
-          shift 1
-          ;;
-          --child)
-          CHILD=1
-          shift 1
-          ;;
-          -q | --quiet)
-          QUIET=1
-          shift 1
-          ;;
-          -s | --strict)
-          STRICT=1
-          shift 1
-          ;;
-          -t)
-          TIMEOUT="$2"
-          if [[ $TIMEOUT == "" ]]; then break; fi
-          shift 2
-          ;;
-          --timeout=*)
-          TIMEOUT="${1#*=}"
-          shift 1
-          ;;
-          --)
-          shift
-          CLI="$@"
-          break
-          ;;
-          --help)
-          usage
-          ;;
-          *)
-          echoerr "Unknown argument: $1"
-          usage
-          ;;
-      esac
-      let index+=1
-  done
-  if [[ ${#HOST[@]} -eq 0 || ${#PORT[@]} -eq 0 ]]; then
-      echoerr "Error: you need to provide a host and port to test."
-      usage
-  fi
+    local index=0
+
+    while [[ $# -gt 0 ]]
+    do
+        case "$1" in
+            *:* )
+                hostport=(${1//:/ })
+                HOST[$index]=${hostport[0]}
+                PORT[$index]=${hostport[1]}
+                SLEEP[$index]=${hostport[2]:-3}
+                shift 1
+                ;;
+            -c)
+                CHILD=1
+                shift 1
+                ;;
+            -q)
+                QUIET=1
+                shift 1
+                ;;
+            -s)
+                STRICT=1
+                shift 1
+                ;;
+            -t)
+                TIMEOUT="$2"
+                if [[ $TIMEOUT == "" ]]; then break; fi
+                shift 2
+                ;;
+            --)
+                shift
+                CLI="$@"
+                break
+                ;;
+            -h)
+                usage
+                ;;
+            *)
+                echoerr "Unknown argument: $1"
+                usage
+                ;;
+        esac
+        let index+=1
+    done
+
+    if [[ ${#HOST[@]} -eq 0 || ${#PORT[@]} -eq 0 ]]; then
+        echoerr "Error: you need to provide a host and port to test."
+        usage
+    fi
 }
 
 iterate_hosts() {
-  local result=0
-  local index=0
-  local wait_function=$1
-  while [[ $result -eq 0 && $index -lt ${#HOST[@]} ]]; do
-    ($wait_function ${HOST[$index]} ${PORT[$index]})
-    result=$?
-    let index+=1
-  done
-  echo $result
+    local result=0
+    local index=0
+    local wait_function=$1
+
+    while [[ $result -eq 0 && $index -lt ${#HOST[@]} ]]; do
+        ($wait_function ${HOST[$index]} ${PORT[$index]} ${SLEEP[$index]})
+        result=$?
+        let index+=1
+    done
+
+    echo $result
 }
 
 wait_for_services() {
-  TIMEOUT=${TIMEOUT:-15}
-  STRICT=${STRICT:-0}
-  CHILD=${CHILD:-0}
-  QUIET=${QUIET:-0}
+    TIMEOUT=${TIMEOUT:-11}
+    STRICT=${STRICT:-0}
+    CHILD=${CHILD:-0}
+    QUIET=${QUIET:-0}
 
-  if [[ $CHILD -gt 0 ]]; then
-      exit $(iterate_hosts wait_for)
-  else
-      if [[ $TIMEOUT -gt 0 ]]; then
-          RESULT=$(iterate_hosts wait_for_wrapper)
-      else
-          RESULT=$(iterate_hosts wait_for)
-      fi
-  fi
+    if [[ $CHILD -gt 0 ]]; then
+        exit $(iterate_hosts wait_for)
+    else
+        if [[ $TIMEOUT -gt 0 ]]; then
+            RESULT=$(iterate_hosts wait_for_wrapper)
+        else
+            RESULT=$(iterate_hosts wait_for)
+        fi
+    fi
 }
 
 parse_arguments "$@"
